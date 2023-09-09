e3987ea71fc6db3abc86cd2fce809eb6a268e5fd
Ahmed Rafik Ibrahim <ahmedrafik_maaar@outlook.com>
2023-09-10 00:39:45 +0200

    refactor: add a type

--- src/CliApp/CliCommands/targetPrice.CliCommand.ts
+++ src/CliApp/CliCommands/targetPrice.CliCommand.ts
@@ -7,7 +7,7 @@ export const targetPriceCli = new Command();
 targetPriceCli.name('target-price')
     .description('Calculates the number of shares you need to buy to satisfy your portfolio with a limited budget')
     .argument('<price>', 'target portfolio price')
-    .action(async (targetPrice) => {
+    .action(async (targetPrice: string) => {
         const useCase = new GetPortfolioWithPrice_UseCase(Number(targetPrice))
         const portfolio = await useCase.handler()
         PortfolioCli_Presenter.present(portfolio)
